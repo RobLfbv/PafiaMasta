51dc3bd73c9fabd8ec0408d1401b683a27f07abd
Robin Lefebvre <robin.lefebvre12@gmail.com>
2024-01-12 09:14:37 +0100

    Init

---
+++ Assets/CharacterBehaviour.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    //private CharacterInput _character
+    void Awake()
+    {
+
+    }
+}
