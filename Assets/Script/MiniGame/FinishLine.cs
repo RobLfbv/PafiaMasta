136aea4f6b7e6985c8d7a59e3e079471964b291f
TaGiOo <alan5.siviniant@gmail.com>
2024-02-27 21:55:30 +0100

    confetti added sur alan5

--- Assets/Script/MiniGame/FinishLine.cs
+++ Assets/Script/MiniGame/FinishLine.cs
@@ -8,12 +8,16 @@ public class FinishLine : MonoBehaviour
     private YetteRunning yette;
     [SerializeField]
     private ChronoBehaviour chrono;
+    [SerializeField]
+    public GameObject particleConfettis;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && GameStateBehaviour.Instance.currentState == GameStateBehaviour.GameState.RunMiniGame)
         {
             chrono.stopTimer = true;
             yette.playerWin = true;
+            particleConfettis.SetActive(true);
         }
     }
 }
