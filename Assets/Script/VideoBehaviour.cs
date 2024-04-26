2cfa432ef4d52fbae5d2fe0fbd0e41f7f0eabfca
RobLfbv <92669641+RobLfbv@users.noreply.github.com>
2024-04-26 21:08:34 +0200

    WEBGL FONCTIONNENENENENNE

--- Assets/Script/VideoBehaviour.cs
+++ Assets/Script/VideoBehaviour.cs
@@ -3,18 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Video;
+using UnityEditor;
 
+[RequireComponent(typeof(VideoPlayer))]
 public class VideoBehaviour : MonoBehaviour
 {
-    public VideoPlayer video;
+    [SerializeField] private string videoFileName;
 
     private void Start()
     {
+        #if UNITY_STANDALONE_WIN
+        PlayerSettings.WebGL.useEmbeddedResources = true;
+        #endif
+        VideoPlayer video = GetComponent<VideoPlayer>();
         video.loopPointReached += ReturnMenu;
+        string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+        print(videoPath);
+        video.url = videoPath;
+        video.Play();
     }
 
     void ReturnMenu(VideoPlayer vp)
     {
         SceneManager.LoadScene(0);
     }
+/*
+    void Start()
+    {
+
+        GameObject camera = GameObject.Find("Main Camera");
+
+        var videoPlayer = camera.AddComponent<VideoPlayer>();
+
+        videoPlayer.playOnAwake = false;
+        videoPlayer.renderMode = VideoRenderMode.CameraNearPlane;
+
+        videoPlayer.targetCameraAlpha = 1F;
+
+        videoPlayer.url = Application.dataPath + "/Resources/" + video.clip.name + ".mp4";
+
+        videoPlayer.isLooping = true;
+
+        videoPlayer.loopPointReached += EndReached;
+
+        videoPlayer.Play();
+    }
+
+    void EndReached(VideoPlayer vp)
+    {
+        SceneManager.LoadScene(0);
+    }*/
 }
